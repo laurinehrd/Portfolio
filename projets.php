eb360c5fbd6b033931b3323b2d65547c01da28d6
Laurine Herard <laurine.herard.akyos@gmail.com>
2021-01-15 16:16:32 +0100

    responsive projects ok

--- projets.php
+++ projets.php
@@ -15,29 +15,125 @@
 
     <div class="content-section">
       <div class="row">
-          <div class="col-lg-6">
-              <div class="projet projet-1">
-
+        <div class="col-lg-6">
+          <div class="projet projet-1">
+            <div class="description">
+              <p>
+                <strong>Réalisations : </strong>
+              </p>
+              <p>
+                <i class="fas fa-angle-right"></i> création d'une maquette sur Adobe XD <br>
+                <i class="fas fa-angle-right"></i> réalisation d'une planche des tendances <br>
+                <i class="fas fa-angle-right"></i> création d'illustrations, visuels et animations <br>
+                <i class="fas fa-angle-right"></i> intégration sur WordPress avec contenu personnalisable <br>
+                <i class="fas fa-angle-right"></i> création de Customs post-types <br>
+              </p>
+              <p>
+                <em>Travail en groupe avec deux camarades de la formation.</em>
+              </p>
+              <br>
+              <p>
+                <strong>Technologies : </strong>
+              </p>
+              <div class="icones">
+                <img src="assets/images/wordpress.png" alt="icone wordpress">
+                <img src="assets/images/php.png" alt="icone php">
+                <img src="assets/images/bootstrap.png" alt="icone bootstrap">
+                <img src="assets/images/xd.png" alt="icone adobe xd">
+                <img src="assets/images/illustrator.png" alt="icone illustrator">   
               </div>
+         
+            </div>
+         
           </div>
-          <div class="col-lg-6">
-              <div class="projet projet-2">
-
+        </div>
+        <div class="col-lg-6">
+          <div class="projet projet-2">
+            <div class="description">
+              <p>
+                <strong>Réalisations : </strong>
+              </p>
+              <p>
+                <i class="fas fa-angle-right"></i> création d'une maquette sur Adobe Xd <br>
+                <i class="fas fa-angle-right"></i> détermination du style graphique : couleurs, images, icones, boutons <br>
+                <i class="fas fa-angle-right"></i> développement de fonctions PHP : <br>
+                  • affichage et navigation des dossiers <br>
+                  • actions : ouvrir, renommer, copier, coller des fichiers <br>
+                  • afficher ou masquer les fichiers cachés <br>
+                  • téléchargement de fichiers <br>
+              </p>
+              <p>
+                <em>Travail en binôme avec une camarade de la formation.</em>
+              </p>
+              <br>
+              <p>
+                <strong>Technologies : </strong>
+              </p>
+              <div class="icones">
+                <img src="assets/images/php.png" alt="icone javascript">
+                <img src="assets/images/javascript.png" alt="icone javascript">
+                <img src="assets/images/xd.png" alt="icone adobe xd">
               </div>
+              
+            </div>
+
           </div>
+        </div>
 
       </div>
       <div class="row">
-          <div class="col-lg-6">
-              <div class="projet projet-3">
-
+        <div class="col-lg-6">
+          <div class="projet projet-3">
+            <div class="description">
+              <p>
+                <strong>Réalisations : </strong>
+              </p>
+              <p>
+                <i class="fas fa-angle-right"></i> choix des images et personnages <br>
+                <i class="fas fa-angle-right"></i> gestion de la gravité <br>
+                <i class="fas fa-angle-right"></i> gestion des collisions <br>
+                <i class="fas fa-angle-right"></i> gestion du mouvement du personnage au clavier <br>
+                <i class="fas fa-angle-right"></i> affichage du score <br>
+                <i class="fas fa-angle-right"></i> ajout de son au différentes actions <br>
+              </p>
+              <br>
+              <p>
+                <strong>Technologies : </strong>
+              </p>
+              <div class="icones">
+                <img src="assets/images/javascript.png" alt="icone javascript">
               </div>
+            </div>
+          
           </div>
-          <div class="col-lg-6">
-              <div class="projet projet-4">
-
+        </div>
+        <div class="col-lg-6">
+          <div class="projet projet-4">
+            <div class="description">
+              <p>
+                <strong>Réalisations : </strong>
+              </p>
+              <p>
+                <i class="fas fa-angle-right"></i> élaboration la charte graphique <br>
+                <i class="fas fa-angle-right"></i> maquettage des pages du site sur Photoshop <br>
+                <i class="fas fa-angle-right"></i> création de la maquette en responsive (version mobile) <br>
+              </p>
+              <p>
+                <em>Travail en groupe avec des camarades de la formation.</em>
+              </p>
+              <br>
+              <p>
+                <strong>Technologies : </strong>
+              </p>
+              <div class="icones">
+                <img src="assets/images/photoshop.png" alt="icone photoshop">
+                <img src="assets/images/trello.png" alt="icone trello">  
               </div>
+                       
+            </div>
+
           </div>
+        </div>
 
       </div>
 
@@ -55,3 +151,5 @@
 
 
 </div>
+
+
